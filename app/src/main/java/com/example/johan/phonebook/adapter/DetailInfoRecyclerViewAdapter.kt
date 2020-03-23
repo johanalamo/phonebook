cce74637b7c291cf4b6b85f8167a2c62f6ae48e1
Johan Alamo <johan.alamo@gmail.com>
2020-03-23 01:25:41 -0300

    update detail View to dataBinding

--- app/src/main/java/com/example/johan/phonebook/adapter/DetailInfoRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/johan/phonebook/adapter/DetailInfoRecyclerViewAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.johan.phonebook.adapter
 
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.johan.phonebook.R
+import com.example.johan.phonebook.databinding.LayoutDetailinfoListViewHolderBinding
 import com.example.johan.phonebook.response.DetailInfo
 import kotlinx.android.synthetic.main.layout_detailinfo_list_view_holder.view.*
 
@@ -17,24 +20,19 @@ class DetailInfoRecyclerViewAdapter(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.layout_detailinfo_list_view_holder,
-            parent,
-            false
-        )
-
-        return ViewHolder(view)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<LayoutDetailinfoListViewHolderBinding>(layoutInflater,
+            R.layout.layout_detailinfo_list_view_holder, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        holder.view.txtTitle.text = data[position].fieldName
-        holder.view.txtExtra.text = data[position].extraInfo
-        holder.view.txtValue.text = data[position].value
+        val info = data[position]
+        holder.binding.info = info
     }
 
     override fun getItemCount() = data.size
 
     //internal classes and interfaces
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    class ViewHolder(val binding: LayoutDetailinfoListViewHolderBinding): RecyclerView.ViewHolder(binding.root)
 }
